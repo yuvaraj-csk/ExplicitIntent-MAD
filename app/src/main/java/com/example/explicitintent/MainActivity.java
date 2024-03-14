bc151e79875e93ed6637095c666d8671b5442b7a
yuvaraj-csk <134052574+yuvaraj-csk@users.noreply.github.com>
2024-03-14 09:39:35 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/explicitintent/MainActivity.java
+++ app/src/main/java/com/example/explicitintent/MainActivity.java
@@ -1,14 +1,29 @@
-package com.example.explicitintent;
+package com.example.explicit_indent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    private EditText numberEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        numberEditText = findViewById(R.id.numberEditText1);
+        Button factorialButton = findViewById(R.id.factorialButton);
+
+        factorialButton.setOnClickListener(v -> {
+            int number = Integer.parseInt(numberEditText.getText().toString());
+
+            Intent intent = new Intent(MainActivity.this, MainActivity3.class);
+            intent.putExtra("number", number);
+            startActivity(intent);
+        });
     }
 }
